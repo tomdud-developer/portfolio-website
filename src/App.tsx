48734af7f1638e607d3fb7f0710f0036febdd2ce
tomdud-developer <tomadud045@student.polsl.pl>
2023-04-15 11:52:32 +0200

    Test of deployment

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, test
+          Learn React
         </a>
       </header>
     </div>
